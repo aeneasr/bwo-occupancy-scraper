72cac732a4dec07e36711a84ec235a6a39bacf60
aeneasr <3372410+aeneasr@users.noreply.github.com>
2020-10-19 15:49:17 +0200

    fix: use real time api

--- scrape.js
+++ scrape.js
@@ -7,14 +7,23 @@ const fs = require('fs');
 
 const destination = 'stats';
 
-fetch('https://www.boulderwelt-muenchen-ost.de/ampel_2/')
-  .then((res) => res.text())
-  .then((html) => {
-    const doc = new JSDOM(html);
-    const occupancy = doc.window.document.querySelector(
-      '#crowd-level-tags-container > div.crowd-level-tag.crowd-level-pointer > div'
-    ).innerHTML;
-
+fetch('https://www.boulderwelt-muenchen-ost.de/wp-admin/admin-ajax.php', {
+  headers: {
+    accept: '*/*',
+    'accept-language': 'de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7',
+    'cache-control': 'no-cache',
+    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    pragma: 'no-cache',
+    'x-requested-with': 'XMLHttpRequest',
+  },
+  body: 'action=cxo_get_crowd_indicator',
+  method: 'POST',
+  mode: 'cors',
+})
+  .then((res) => res.json())
+  .then((stats) => {
+    const { queue, percent } = stats;
+    console.log(stats);
     const now = new Date(
       new Date().toLocaleString('en-US', { timeZone: 'Europe/Amsterdam' })
     );
@@ -23,8 +32,8 @@ fetch('https://www.boulderwelt-muenchen-ost.de/ampel_2/')
       const fp = path.join(destination, 'total.csv');
 
       const data = [
-        ['Date', 'Occupancy'],
-        [now.toISOString(), occupancy],
+        ['Date', 'Occupancy', 'Waiting Queue'],
+        [now.toISOString(), percent / 100.0, queue],
       ];
 
       if (fs.existsSync(fp)) {
